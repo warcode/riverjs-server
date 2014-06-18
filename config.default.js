35adacc8012e2fd4da1fd05c2c4ad4664d721ab2
warcode <warcode@deny.io>
2014-06-18 17:38:16 +0200

    semicolons

--- config.default.js
+++ config.default.js
@@ -1,11 +1,11 @@
-var config = {}
+var config = {};
 
 config.twitter = {};
 config.web = {};
 
 config.twitter.apikey = 'apikey';
 config.twitter.apisecret =  'apisecret';
-config.twitter.oauthauthorizeurl = 'oauthauthorizeurl'
+config.twitter.oauthauthorizeurl = 'oauthauthorizeurl';
 config.web.port = 55655;
 
 module.exports = config;
